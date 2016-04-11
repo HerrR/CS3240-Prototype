499533fb78633d9e157b2f775feeb86df975e270
josefinemsson <josmar@kth.se>
2016-04-11 16:07:32 +0800

    Jag orkar inte Rickard

--- js/app.js
+++ js/app.js
@@ -3,115 +3,115 @@ var app = angular.module('prototype', ['ngRoute', 'ngCookies']);
 // 'ngAnimate',
 
 app.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-		when('/', {
-			templateUrl: 'partials/homeScreen.html',
-			controller: 'homeScreenCtrl',
-			redirectURL: '/',
-			requirements: null,
-			status_message: "Hello World!"
-		}).
-		when('/booking', {
-			templateUrl: 'partials/booking.html',
-			controller: 'bookingCtrl',
-			redirectURL: '/',
-			requirements: null,
-			status_message: "Hello World 2!"
-		}).
-		when('/confirm',{
-			templateUrl: 'partials/confirm.html',
-			controller: 'confirmCtrl',
-			redirectURL: '/booking',
-			requirements: ["activeBooking"],
-			status_message: "Hello World 3!"
-		}).
-		when('/unlock', {
-			templateUrl: 'partials/unlock.html',
-			controller: 'unlockCtrl',
-			redirectURL: '/myBooking',
-			requirements: ["activeBooking"],
-			status_message: "Hello World!"
-		}).
-		when('/destinations',{
-			templateUrl: 'partials/destinations.html',
-			controller: 'destinationCtrl',
-			redirectURL: '/',
-			requirements: ["activeBooking", "unlockedBike"],
-			status_message: "Hello World!"
-		}).
-		when('/navigation',{
-			templateUrl: 'partials/navigation.html',
-			controller: 'navigationCtrl',
-			redirectURL: '/',
-			requirements: ["activeBooking", "unlockedBike", "activeDestination"],
-			status_message: "Hello World!"
-		}).
-		when('/return',{
-			templateUrl: 'partials/return.html',
-			controller: 'returnCtrl',
-			redirectURL: '/',
-			requirements: ["activeBooking", "unlockedBike"],
-			status_message: "Hello World!"
-		}).
-		when('/success',{
-			templateUrl: 'partials/success.html',
-			controller: 'successCtrl',
-			redirectURL: '/',
-			requirements: ["activeBooking", "unlockedBike"],
-			status_message: "Hello World!"
-		}).
-		when('/myRide',{
-			templateUrl: 'partials/ride.html',
-			controller: 'rideCtrl',
-			redirectURL: '/',
-			requirements: ["unlockedBike"],
-			status_message: "Hello World!"
-		}).
-		when('/myBooking',{
-			templateUrl: 'partials/activeBooking.html',
-			controller: 'activeBookingCtrl',
-			redirectURL: '/',
-			requirements: ["activeBooking"],
-			status_message: "Hello World!"
-		}).
-		otherwise({
-			redirectTo: "/"
-		})
+	function ($routeProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'partials/homeScreen.html',
+            controller: 'homeScreenCtrl',
+            redirectURL: '/',
+            requirements: null,
+            status_message: "Hello!"
+        }).
+        when('/booking', {
+            templateUrl: 'partials/booking.html',
+            controller: 'bookingCtrl',
+            redirectURL: '/',
+            requirements: null,
+            status_message: "Where do you want to pick up your bike?"
+        }).
+        when('/confirm', {
+            templateUrl: 'partials/confirm.html',
+            controller: 'confirmCtrl',
+            redirectURL: '/booking',
+            requirements: ["activeBooking"],
+            status_message: "Are you sure?"
+        }).
+        when('/unlock', {
+            templateUrl: 'partials/unlock.html',
+            controller: 'unlockCtrl',
+            redirectURL: '/myBooking',
+            requirements: ["activeBooking"],
+            status_message: "Unlock your bike"
+        }).
+        when('/destinations', {
+            templateUrl: 'partials/destinations.html',
+            controller: 'destinationCtrl',
+            redirectURL: '/',
+            requirements: ["activeBooking", "unlockedBike"],
+            status_message: "Where do you want to go?"
+        }).
+        when('/navigation', {
+            templateUrl: 'partials/navigation.html',
+            controller: 'navigationCtrl',
+            redirectURL: '/',
+            requirements: ["activeBooking", "unlockedBike", "activeDestination"],
+            status_message: "Navigation"
+        }).
+        when('/return', {
+            templateUrl: 'partials/return.html',
+            controller: 'returnCtrl',
+            redirectURL: '/',
+            requirements: ["activeBooking", "unlockedBike"],
+            status_message: "Return bike"
+        }).
+        when('/success', {
+            templateUrl: 'partials/success.html',
+            controller: 'successCtrl',
+            redirectURL: '/',
+            requirements: ["activeBooking", "unlockedBike"],
+            status_message: "Bike is returned!"
+        }).
+        when('/myRide', {
+            templateUrl: 'partials/ride.html',
+            controller: 'rideCtrl',
+            redirectURL: '/',
+            requirements: ["unlockedBike"],
+            status_message: "Ride Overview"
+        }).
+        when('/myBooking', {
+            templateUrl: 'partials/activeBooking.html',
+            controller: 'activeBookingCtrl',
+            redirectURL: '/',
+            requirements: ["activeBooking"],
+            status_message: "Go unlock your bike!"
+        }).
+        otherwise({
+            redirectTo: "/"
+        })
 	}
 ])
 
-.run(function($rootScope, $location, Model){
-	$rootScope.$on( "$routeChangeStart", function(event, next, current) {
-		var routeRequirements = next.$$route.requirements;
-		var hasActiveBooking = !jQuery.isEmptyObject(Model.getBooking().location);
-		var hasActiveRide = !(Model.getBooking().unlockedAt == null);
-		var hasActiveDestination = !(Model.getDestination() == null);
+.run(function ($rootScope, $location, Model) {
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        var routeRequirements = next.$$route.requirements;
+        var hasActiveBooking = !jQuery.isEmptyObject(Model.getBooking().location);
+        var hasActiveRide = !(Model.getBooking().unlockedAt == null);
+        var hasActiveDestination = !(Model.getDestination() == null);
 
-		if(next.$$route.originalPath == "/"){
-			if(hasActiveBooking && hasActiveRide){
-				$location.path("/myRide");
-			} else if (hasActiveBooking && !hasActiveRide){
-				$location.path("/myBooking");
-			}
-		};
+        if (next.$$route.originalPath == "/") {
+            if (hasActiveBooking && hasActiveRide) {
+                $location.path("/myRide");
+            } else if (hasActiveBooking && !hasActiveRide) {
+                $location.path("/myBooking");
+            }
+        };
 
-		if(routeRequirements != null){
-			for(requirement in routeRequirements){
-				if(routeRequirements[requirement] == "activeBooking"){
-					if(!hasActiveBooking){
-						$location.path(next.$$route.redirectURL);
-					}
-				} else if(routeRequirements[requirement] == "unlockedBike"){
-					if(!hasActiveRide){
-						$location.path(next.$$route.redirectURL);
-					}
-				} else if(routeRequirements[requirement] == "activeDestination"){
-					if(!hasActiveDestination){
-						$location.path(next.$$route.redirectURL);
-					}
-				}
-			}
-		}
+        if (routeRequirements != null) {
+            for (requirement in routeRequirements) {
+                if (routeRequirements[requirement] == "activeBooking") {
+                    if (!hasActiveBooking) {
+                        $location.path(next.$$route.redirectURL);
+                    }
+                } else if (routeRequirements[requirement] == "unlockedBike") {
+                    if (!hasActiveRide) {
+                        $location.path(next.$$route.redirectURL);
+                    }
+                } else if (routeRequirements[requirement] == "activeDestination") {
+                    if (!hasActiveDestination) {
+                        $location.path(next.$$route.redirectURL);
+                    }
+                }
+            }
+        }
     });
 });
--- js/controllers.js
+++ js/controllers.js
@@ -2,36 +2,36 @@ app.controller('AppCtrl', function ($scope, Model, $location, $window) {
     $scope.mapsInitialized = false;
     $scope.positionUpdated = false;
 
-    $window.googleMapsInitialized = function(){
+    $window.googleMapsInitialized = function () {
         console.log("Google Maps initialized!");
         $scope.mapsInitialized = true;
         $scope.updateDistances(Model.getUserPosition());
     };
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
+        navigator.geolocation.getCurrentPosition(function (position) {
             $scope.positionUpdated = true;
-            $scope.$apply(function(){
+            $scope.$apply(function () {
                 Model.setUserPosition(position.coords);
             });
         });
     };
 
-    $scope.$watch(function(){
-        return Model.getUserPosition();
-    },
-    function(newVal){
-        if($scope.mapsInitialized){
-            $scope.updateDistances(newVal);  
-        };
-    });
+    $scope.$watch(function () {
+            return Model.getUserPosition();
+        },
+        function (newVal) {
+            if ($scope.mapsInitialized) {
+                $scope.updateDistances(newVal);
+            };
+        });
 
-    $scope.updateDistances = function(newPosition){
+    $scope.updateDistances = function (newPosition) {
         var directionsService = new google.maps.DirectionsService();
         var stations = Model.allStations();
         var currentPosition = new google.maps.LatLng(newPosition.latitude, newPosition.longitude);
 
-        for(i in stations){
+        for (i in stations) {
             var start = currentPosition;
             var end = new google.maps.LatLng(stations[i].coordinates.latitude, stations[i].coordinates.longitude);
 
@@ -39,7 +39,7 @@ app.controller('AppCtrl', function ($scope, Model, $location, $window) {
         }
     }
 
-    var findNewDistance = function(start, end, id, directionsService){
+    var findNewDistance = function (start, end, id, directionsService) {
         // console.log(start, end, id, directionsService);
         var request = {
             origin: start,
@@ -47,22 +47,21 @@ app.controller('AppCtrl', function ($scope, Model, $location, $window) {
             travelMode: google.maps.TravelMode.WALKING
         };
 
-        directionsService.route(request, function(result, status) {
+        directionsService.route(request, function (result, status) {
             if (status == google.maps.DirectionsStatus.OK) {
                 Model.updateDistanceInfo(id, result.routes[0].legs[0].distance.value);
                 $scope.$apply();
             } else {
-                console.log("Failed to get route somewhere ",status);
+                console.log("Failed to get route somewhere ", status);
             }
         });
     };
-    $scope.$on("$routeChangeStart", function(event, next, current){
+    $scope.$on("$routeChangeStart", function (event, next, current) {
         $scope.statusMessage = next.$$route.status_message;
     });
 });
 
-app.controller('homeScreenCtrl', function ($scope, Model, $location) {
-});
+app.controller('homeScreenCtrl', function ($scope, Model, $location) {});
 
 app.controller('bookingCtrl', function ($scope, Model, $location) {
     // Cancel the booking if there is already one
@@ -73,22 +72,25 @@ app.controller('bookingCtrl', function ($scope, Model, $location) {
     $scope.locations = Model.allStations();
 
     // When the user clicks
-    $scope.bookBike = function(location){
+    $scope.bookBike = function (location) {
         var chosenLocation = location;
-        var bestBike = {id: null, battery: 0};
+        var bestBike = {
+            id: null,
+            battery: 0
+        };
 
-        for (bike in location.availableBikes){
-            if(location.availableBikes[bike].battery > bestBike.battery){
+        for (bike in location.availableBikes) {
+            if (location.availableBikes[bike].battery > bestBike.battery) {
                 bestBike = location.availableBikes[bike];
             }
         }
 
         Model.initializeBooking(location, bestBike);
-        
+
         $location.path("/confirm");
     };
 
-    $scope.initializeMap = function(){
+    $scope.initializeMap = function () {
         var pos = Model.getUserPosition();
         var currentPosition = new google.maps.LatLng(pos.latitude, pos.longitude);
         $scope.map = new google.maps.Map(document.getElementById('map'), {
@@ -103,12 +105,12 @@ app.controller('bookingCtrl', function ($scope, Model, $location) {
             title: "User location"
         });
 
-        for(var location in $scope.locations){
+        for (var location in $scope.locations) {
             var numAvailableBikes = $scope.locations[location].availableBikes.length;
 
             var image = {
-              url: "img/bicons/bike-icon-"+numAvailableBikes+".png",
-              scaledSize: new google.maps.Size(50, 50)
+                url: "img/bicons/bike-icon-" + numAvailableBikes + ".png",
+                scaledSize: new google.maps.Size(50, 50)
             };
 
             var marker = new google.maps.Marker({
@@ -120,9 +122,9 @@ app.controller('bookingCtrl', function ($scope, Model, $location) {
                 location: $scope.locations[location]
             });
 
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 var location = this["location"];
-                $scope.$apply(function(){
+                $scope.$apply(function () {
                     $scope.bookBike(location);
                 });
             });
@@ -130,10 +132,10 @@ app.controller('bookingCtrl', function ($scope, Model, $location) {
     };
 
     $scope.$watch(
-        function(){
-            return Model.getUserPosition() 
-        }, 
-        function(newVal){
+        function () {
+            return Model.getUserPosition()
+        },
+        function (newVal) {
             var newLocation = new google.maps.LatLng(newVal.latitude, newVal.longitude)
             $scope.map.panTo(newLocation);
             $scope.userPositionMarker.setPosition(newLocation);
@@ -151,8 +153,8 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
         seconds: 0
     }
 
-    $scope.timesUp = function(){
-        if(($scope.timeleft.minutes < 1) && ($scope.timeleft.seconds < 1)){
+    $scope.timesUp = function () {
+        if (($scope.timeleft.minutes < 1) && ($scope.timeleft.seconds < 1)) {
             // console.log("Times up!");
             return true;
         } else {
@@ -161,15 +163,15 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
         }
     }
 
-    $scope.msecToMinSec = function(msec){
+    $scope.msecToMinSec = function (msec) {
         return {
-            minutes: Math.floor(msec/1000/60),
-            seconds: Math.floor((msec/1000)%60),
+            minutes: Math.floor(msec / 1000 / 60),
+            seconds: Math.floor((msec / 1000) % 60),
         };
     }
 
-    $scope.showWarning = function(){
-        if($scope.timeleft.minutes < 0){
+    $scope.showWarning = function () {
+        if ($scope.timeleft.minutes < 0) {
             Model.cancelBooking();
             return true;
         } else {
@@ -177,17 +179,17 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
         }
     }
 
-    timer = $interval(function(){
+    timer = $interval(function () {
         var msecLeft = $scope.timeConstraints.reservationTime - (new Date() - Model.getBooking().bookedAt);
         $scope.timeleft = $scope.msecToMinSec(msecLeft);
     }, 1000);
-    
 
-    $scope.booking = function(){
+
+    $scope.booking = function () {
         return Model.getBooking();
     }
 
-    $scope.confirmCancellation = function(){
+    $scope.confirmCancellation = function () {
         if (confirm("Are you sure you want to cancel your booking?") == true) {
             Model.cancelBooking();
             $location.path("/");
@@ -196,7 +198,7 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
 
     var currentPosition = new google.maps.LatLng(Model.getUserPosition().latitude, Model.getUserPosition().longitude);
     var bookingLocation = new google.maps.LatLng($scope.booking().location.coordinates.latitude, $scope.booking().location.coordinates.longitude);
-        
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: currentPosition,
         zoom: 16
@@ -212,7 +214,7 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
         travelMode: google.maps.TravelMode.WALKING
     };
 
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(result);
         }
@@ -220,7 +222,7 @@ app.controller('activeBookingCtrl', function ($scope, Model, $location, $interva
 });
 
 app.controller('rideCtrl', function ($scope, Model, $location, $interval) {
-   var timer;
+    var timer;
     $scope.timeConstraints = Model.getTimeConstraints();
 
     $scope.startpageState = "default";
@@ -230,8 +232,8 @@ app.controller('rideCtrl', function ($scope, Model, $location, $interval) {
         seconds: 0
     };
 
-    $scope.timesUp = function(){
-        if(($scope.timeleft.minutes < 1) && ($scope.timeleft.seconds < 1)){
+    $scope.timesUp = function () {
+        if (($scope.timeleft.minutes < 1) && ($scope.timeleft.seconds < 1)) {
             // console.log("Times up!");
             return true;
         } else {
@@ -240,16 +242,16 @@ app.controller('rideCtrl', function ($scope, Model, $location, $interval) {
         }
     };
 
-    $scope.msecToMinSec = function(msec){
+    $scope.msecToMinSec = function (msec) {
         return {
-            minutes: Math.floor(msec/1000/60),
-            seconds: Math.floor((msec/1000)%60),
+            minutes: Math.floor(msec / 1000 / 60),
+            seconds: Math.floor((msec / 1000) % 60),
         };
     };
 
-    $scope.showWarning = function(){
-        if($scope.timeleft.minutes < 0){
-            if($scope.startpageState == "confirmBooking"){
+    $scope.showWarning = function () {
+        if ($scope.timeleft.minutes < 0) {
+            if ($scope.startpageState == "confirmBooking") {
                 Model.cancelBooking();
             }
             return true;
@@ -258,16 +260,16 @@ app.controller('rideCtrl', function ($scope, Model, $location, $interval) {
         }
     };
 
-    timer = $interval(function(){
+    timer = $interval(function () {
         var msecLeft = $scope.timeConstraints.rentalTime - (new Date() - Model.getBooking().unlockedAt);
         $scope.timeleft = $scope.msecToMinSec(msecLeft);
     }, 1000);
-    
-    $scope.bookingConfirmed = function(){
-        return Model.checkBookingConfirmation();   
+
+    $scope.bookingConfirmed = function () {
+        return Model.checkBookingConfirmation();
     };
 
-    $scope.booking = function(){
+    $scope.booking = function () {
         return Model.getBooking();
     };
 });
@@ -275,12 +277,12 @@ app.controller('rideCtrl', function ($scope, Model, $location, $interval) {
 app.controller('confirmCtrl', function ($scope, Model, $location) {
 
     // Sets current time as bookedAt
-    $scope.confirmBooking = function(){
+    $scope.confirmBooking = function () {
         Model.confirmBooking();
     };
 
     // Get booking
-    $scope.booking = function(){
+    $scope.booking = function () {
         return Model.getBooking();
     };
 
@@ -289,7 +291,7 @@ app.controller('confirmCtrl', function ($scope, Model, $location) {
 app.controller('unlockCtrl', function ($scope, Model, $location) {
 
     $scope.stationNumber = Model.getBooking().bikeSlot;
-    $scope.unlockBike = function(){
+    $scope.unlockBike = function () {
         Model.unlockBike();
     }
 });
@@ -297,7 +299,7 @@ app.controller('unlockCtrl', function ($scope, Model, $location) {
 app.controller('destinationCtrl', function ($scope, Model, $location) {
     $scope.destinations = Model.allStations();
 
-    $scope.bookReturnAt = function(dest){
+    $scope.bookReturnAt = function (dest) {
         console.log("Chose destination", dest);
         Model.setDestination(dest);
         $location.path("/navigation");
@@ -305,7 +307,7 @@ app.controller('destinationCtrl', function ($scope, Model, $location) {
 
     $scope.userPositionMarker;
 
-    $scope.initializeMap = function(){
+    $scope.initializeMap = function () {
         var pos = Model.getUserPosition();
         var currentPosition = new google.maps.LatLng(pos.latitude, pos.longitude);
         $scope.map = new google.maps.Map(document.getElementById('map'), {
@@ -320,12 +322,12 @@ app.controller('destinationCtrl', function ($scope, Model, $location) {
             title: "User location"
         });
 
-        for(var destination in $scope.destinations){
+        for (var destination in $scope.destinations) {
             var numAvailableSlots = $scope.destinations[destination].numSlots - $scope.destinations[destination].availableBikes.length;
 
             var image = {
-              url: "img/bicons/bike-icon-"+numAvailableSlots+".png",
-              scaledSize: new google.maps.Size(50, 50)
+                url: "img/bicons/bike-icon-" + numAvailableSlots + ".png",
+                scaledSize: new google.maps.Size(50, 50)
             };
 
             var marker = new google.maps.Marker({
@@ -337,9 +339,9 @@ app.controller('destinationCtrl', function ($scope, Model, $location) {
                 location: $scope.destinations[destination]
             });
 
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 var location = this["location"];
-                $scope.$apply(function(){
+                $scope.$apply(function () {
                     $scope.bookReturnAt(location);
                 });
             });
@@ -347,10 +349,10 @@ app.controller('destinationCtrl', function ($scope, Model, $location) {
     };
 
     $scope.$watch(
-        function(){
-            return Model.getUserPosition() 
-        }, 
-        function(newVal){
+        function () {
+            return Model.getUserPosition()
+        },
+        function (newVal) {
             var newLocation = new google.maps.LatLng(newVal.latitude, newVal.longitude)
             $scope.map.panTo(newLocation);
             $scope.userPositionMarker.setPosition(newLocation);
@@ -362,13 +364,13 @@ app.controller('destinationCtrl', function ($scope, Model, $location) {
 app.controller('navigationCtrl', function ($scope, Model, $location) {
     $scope.destination = Model.getDestination();
 
-    $scope.unsetDestination = function(){
+    $scope.unsetDestination = function () {
         Model.unsetDestination();
     };
 
     var currentPosition = new google.maps.LatLng(Model.getUserPosition().latitude, Model.getUserPosition().longitude);
     var dropoffDestination = new google.maps.LatLng($scope.destination.coordinates.latitude, $scope.destination.coordinates.longitude);
-        
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: currentPosition,
         zoom: 16
@@ -384,7 +386,7 @@ app.controller('navigationCtrl', function ($scope, Model, $location) {
         travelMode: google.maps.TravelMode.DRIVING
     };
 
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(result);
             // console.log(result);
@@ -398,28 +400,25 @@ app.controller('navigationCtrl', function ($scope, Model, $location) {
 app.controller('returnCtrl', function ($scope, Model, $location) {
     $scope.retardedUser = true;
 
-    $scope.userNotRetarded = function(){
+    $scope.userNotRetarded = function () {
         return !$scope.retardedUser;
     }
-    $scope.userIsRetarded = function(val){
+    $scope.userIsRetarded = function (val) {
         $scope.retardedUser = val;
     }
+
 });
 
 app.controller('successCtrl', function ($scope, Model, $location) {
     var chargePerHourOverdue = Model.getChargeRates();
     var msecLeft = Model.getTimeConstraints().rentalTime - (new Date() - Model.getBooking().unlockedAt);
-    
-    if(msecLeft >= 0){
+
+    if (msecLeft >= 0) {
         $scope.rideCost = 0;
     } else {
-        var penaltyCharge = Math.abs(msecLeft)/1000/60/60 * chargePerHourOverdue;
-        $scope.rideCost = Math.round(penaltyCharge*100)/100;
+        var penaltyCharge = Math.abs(msecLeft) / 1000 / 60 / 60 * chargePerHourOverdue;
+        $scope.rideCost = Math.round(penaltyCharge * 100) / 100;
     }
 
     Model.lockBike();
 });
-
-
-
-
